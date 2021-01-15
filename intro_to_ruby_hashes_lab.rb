23610925fee9c85ea193b82d9612c134c8fea092
Gus Bikos <gusbikos@gmail.com>
2021-01-15 20:56:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,9 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => {
-      pieces: "4"
-      rent_in_dollars: {}
-      names: {}
-
-
+      :pieces => "4"
+      :rent_in_dollars => {}
+      :names => {}
     }
   }
 end
